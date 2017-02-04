887a080e428da1602a41c8063597e0c7b8d51868
Peter Ivanov <ivanovp@gmail.com>
2017-02-04 20:15:04 +0100

    Version was increased.

--- version.h
+++ version.h
@@ -5,7 +5,7 @@
 
 #define VER_PRODUCT_MAJOR           0
 #define VER_PRODUCT_MINOR           1
-#define VER_PRODUCT_RELEASE         9
+#define VER_PRODUCT_RELEASE         10
 
 #ifdef QT_DEBUG
 #define VER_PRODUCT_BUILD           99
